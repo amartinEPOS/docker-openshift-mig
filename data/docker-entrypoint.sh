7fb2faad1eacc430815194868c1e512998de4f7e
Martin, Alex <alex.martin@epos-cat.de>
2017-09-28 10:55:07 +0200

    change oc url

--- data/docker-entrypoint.sh
+++ data/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # OC_RSYNC_SRC: rsync source target
 # OC_LOGIN_TOKEN: login token
 
-/oc-client/oc login https://master8.tsi-af.de --token=$OC_LOGIN_TOKEN
+/oc-client/oc login 10.131.0.1:8443 --token=$OC_LOGIN_TOKEN
 /oc-client/oc project atlassian-epos-docker-test
 
 if [ -z "$(ls -A /data)" ]; then
